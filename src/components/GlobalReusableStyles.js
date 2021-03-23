945b4e3b8d39415c2e323aad2973d1e84d90f857
yuri-raiter <yurinraiter@gmail.com>
2021-03-23 19:54:10 -0300

    update hamburguer menu

--- src/components/GlobalReusableStyles.js
+++ src/components/GlobalReusableStyles.js
@@ -1,5 +1,21 @@
 import styled from 'styled-components'
 
+export const StyledLogo = styled.div`
+    display: flex;
+    align-items: center;
+    color: #fff;
+
+    ion-icon {
+        font-size: 2.3rem;
+        margin-right: 0.3rem;
+    }
+
+    h2 {
+        text-transform: uppercase;
+        font-size: 1.3rem;
+    }
+`
+
 export const ButtonWrapper = styled.ul`
     list-style: none;
     display: flex;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,21 +3,46 @@ import React from 'react'
 import styled from 'styled-components'
 import Logo from './Logo'
 
+import { StyledLogo, ButtonWrapper, StyledDownloadButton } from './GlobalReusableStyles'
+
 
 const Nav = () => {  
     return (
         <StyledNav>
             <a href="/">
+            <StyledLogo>
                 <Logo />
+            </StyledLogo>
+            
             </a>
-            <List>
-                <li><a href="/">Download</a></li>
-                <li><a href="/">Why Discord?</a></li>
-                <li><a href="/">Nitro</a></li>
-                <li><a href="/">Safety</a></li>
-                <li><a href="/">Suport</a></li>
-            </List>
-            <Button>Login</Button>
+            <MobileMenu>
+                <TopWrapper>
+                    <a href="/">
+                        <MobileLogo>
+                            <Logo />
+                        </MobileLogo>
+                    </a>
+                    <ion-icon name="close-sharp"></ion-icon>
+                </TopWrapper>
+                <List>
+                    <HomeLink><a href="/">Home</a></HomeLink>
+                    <li><a href="/">Download</a></li>
+                    <li><a href="/">Why Discord?</a></li>
+                    <li><a href="/">Nitro</a></li>
+                    <li><a href="/">Safety</a></li>
+                    <li><a href="/">Suport</a></li>
+                </List>
+                <HamburguerMenuButton>
+                    <DownloadButton>
+                        <ion-icon name="download-outline" />
+                        Download for Windows
+                    </DownloadButton>
+                </HamburguerMenuButton>
+            </MobileMenu>
+            <Wrapper>
+                <Button>Login</Button>
+                <ion-icon name="menu"></ion-icon>
+            </Wrapper>
         </StyledNav>
     )
 }
@@ -28,6 +53,52 @@ const StyledNav = styled.nav`
     display: flex;
     justify-content: space-between;
     align-items: center;
+`
+
+const MobileMenu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @media (max-width: 1023px) {
+        position: fixed;
+        right: 0;
+        top: 0;
+        background: #fff;
+        height: 100vh;
+        width: 315px;
+        z-index: 2;
+        flex-direction: column;
+        justify-content: flex-start;
+        border-radius: 0.5rem 0 0 0.5rem;
+        padding: 1.3rem 3rem 1.3rem 1.3rem;
+    }
+`
+
+const TopWrapper = styled.div`
+    display: none;
+
+    @media (max-width: 1023px) {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+        padding-bottom: 1.8rem;
+        border-bottom: 1px solid #EBEDEF;
+
+        ion-icon {
+            color: #36393F;
+
+            &:nth-child(2) {
+                font-size: 1.5rem;
+            }
+        }
+    }
+    
+`
+
+const MobileLogo = styled(StyledLogo)`
+    color: #36393F;
 `
 
 const List = styled.ul`
@@ -49,6 +120,73 @@ const List = styled.ul`
             }
         }
     }
+
+    @media (max-width: 1023px) {
+        flex-direction: column;
+        width: 100%;
+        margin-top: 1rem;
+
+        li {
+            padding: 0.8rem;
+
+            &:nth-child(-n + 4) { // the first four
+                margin-right: 0;
+            }
+        }
+
+         li a {
+            font-weight: 500;
+            font-size: 0.85rem;
+            color: #36393F;
+        }
+    }
+`
+
+const HomeLink = styled.li`
+    display: none;
+
+    @media (max-width: 1023px) {
+        display: flex;
+        background: #F6F6F6;
+        border-radius: 1rem;
+        
+        a:hover {
+            color: #7289D4;
+        }
+    }
+`
+
+const HamburguerMenuButton = styled(ButtonWrapper)`
+    display: none;
+
+    @media (max-width: 1023px) {
+        display: flex;
+
+        li {
+            padding: 0.3rem 2rem;
+            font-size: 0.8rem;
+        }
+    }
+`
+
+const DownloadButton = styled(StyledDownloadButton)`
+    margin-right: 0;
+`
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+
+    ion-icon {
+        font-size: 2rem;
+        color: #fff;
+        cursor: pointer;
+        display: none;
+
+        @media (max-width: 1023px) {
+            display: flex;
+        }
+    }
 `
 
 const Button = styled.button`
@@ -59,6 +197,7 @@ const Button = styled.button`
     padding: 11px 15px;
     color: #36393F;
     transition: 0.2s all ease;
+    margin-right: 1rem;
 
     &:hover {
         color: #638BFB;
